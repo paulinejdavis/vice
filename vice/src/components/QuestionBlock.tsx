5c6391d1a3b18c36bf2481b7009358d1849b96f9
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-03-03 13:30:11 +0000

    added component elements

--- vice/src/components/QuestionBlock.tsx
+++ vice/src/components/QuestionBlock.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Question } from '../../interfaces';
 
 
-const QuestionBlock = () => {
+const QuestionBlock = ({ 
+  question 
+
+}: {
+  question: Question
+}) => {
   return (
-    <div>
-    </div>
+    <button>
+      className="question-block"
+      {/* onClick={handleClick} */}
+
+      <img src={question.image} alt={question.text} />
+      <h3>{question.text}</h3>
+      <p>
+        <a href={question.image}>alt={question.alt} </a>
+        <a></a>
+      </p>
+    </button>
   )
 }
 
--- vice/src/components/QuestionsBlock.tsx
+++ vice/src/components/QuestionsBlock.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Content } from '../../interfaces';
+import { Content, Question } from '../../interfaces';
+import  QuestionBlock  from './QuestionBlock';
 
 
 const QuestionsBlock = ({
-  quizItem
+  quizItem,
 } : {
   quizItem: Content
 }) => {
+
+
   return (
     <>
-    <h2 id={String(quizItem.id)}>{quizItem.text}</h2>
+      <h2 id={String(quizItem.id)}>{quizItem.text}</h2>
+      <div className="questions-container">
+          {quizItem?.questions.map((question: Question, _index : number)  => (
+              <QuestionBlock
+                key={_index}
+                question={question}
+              />
+          ))}
+
+      </div>
     </>
+
   )
 }
 
